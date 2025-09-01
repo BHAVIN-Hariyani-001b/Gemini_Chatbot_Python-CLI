ae791790e9586a4007b81962098addb49eab0824
BHAVIN-Hariyani-001b <bhedit367@gmail.com>
2025-09-01 16:03:14 +0530

    edit file

--- chatBot_use_gemini.py
+++ chatBot_use_gemini.py
@@ -1,7 +1,7 @@
 from google import genai
 
 # The client gets the API key from the environment variable `GEMINI_API_KEY`.
-client = genai.Client(api_key="<key>")
+client = genai.Client(api_key="AIzaSyDTNBYoHPakg9wF0nnx1dlFJpNXPCPx3o8")
 
 while True:
     user = input("You : ")
